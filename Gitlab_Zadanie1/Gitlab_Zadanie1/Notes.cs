22d613c4d6ddc7d6b60e41e390eec4ee60c69884
pawelprojext <mailkaliego@gmail.com>
2023-02-17 12:15:31 +0100

    GitLab task almost done

---
+++ Gitlab_Zadanie1/Gitlab_Zadanie1/Notes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitlab
+{
+    class Author
+    {
+        public string Name { get; set; }
+    }
+    internal class Notes
+    {
+        public string body { get; set; }
+        public Author author { get; set; }
+    }
+}
