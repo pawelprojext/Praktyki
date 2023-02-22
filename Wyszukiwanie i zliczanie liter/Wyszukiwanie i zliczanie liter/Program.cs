47b9704b5973d7af0691c0ae5d2b0b67e3741c91
pawelprojext <mailkaliego@gmail.com>
2023-02-22 08:44:06 +0100

    task done

---
+++ Wyszukiwanie i zliczanie liter/Wyszukiwanie i zliczanie liter/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        string path = "C:\\Users\\Xopero\\Desktop\\text.txt";
+
+       string text = File.ReadAllText(path);
+        
+        Console.WriteLine("Jaka literę chcesz zliczyć? ");
+        char SearchingFor = Console.ReadKey().KeyChar;
+        Console.WriteLine("\nSzukanie: " + SearchingFor);
+        int howManyLetters = 0;
+
+        foreach(char letter in text)
+        {
+            if (letter == SearchingFor)
+            {
+                howManyLetters++;
+            }
+            
+        }
+        Console.WriteLine("Litera: " + SearchingFor + " Występuję: " + howManyLetters);
+    }
+    
+
+
+}
