6dd9ef37fdf747d1c6ae17a6c1b255dc151c7673
pawelprojext <mailkaliego@gmail.com>
2023-02-23 08:49:29 +0100

    task with strings done

--- Wyszukiwanie i zliczanie liter/Wyszukiwanie i zliczanie liter/Program.cs
+++ Wyszukiwanie i zliczanie liter/Wyszukiwanie i zliczanie liter/Program.cs
@@ -5,29 +5,32 @@
         
         string path = "C:\\Users\\Xopero\\Desktop\\text.txt";
 
-        Console.WriteLine("Jaka literę chcesz zliczyć? ");
-        char SearchingFor = Console.ReadKey().KeyChar;
+        Console.WriteLine("Jaka frazę chcesz zliczyć? ");
+        string SearchingFor = Console.ReadLine();
 
         Console.WriteLine("\nSzukanie: " + SearchingFor);
         int howManyLetters = 0;
 
-        using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+   using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
    using (BufferedStream bs = new BufferedStream(fs))
    using (StreamReader sr = new StreamReader(bs))
    {
        string line;
        while ((line = sr.ReadLine()) != null)
        {
-           foreach(var letter in line)
+                    
+             string[] subs = line.Split(' ');
+                foreach (var sub in subs)
                 {
-                    if (letter == SearchingFor)
+                    if (SearchingFor == sub)
                     {
                         howManyLetters++;
                     }
                 }
             }
-            Console.WriteLine("Litera: " + SearchingFor + " Występuję: " + howManyLetters);
-        }
-    } 
+       }
+            
+        Console.WriteLine("Fraza: " + SearchingFor + " Występuję: " + howManyLetters);
     }
-//}
+ } 
+   
